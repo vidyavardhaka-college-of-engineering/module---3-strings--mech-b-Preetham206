63c286a07df6f47df56fc925671ba88b165f28c1
Preetham206 <20ME0040@vvce.ac.in>
2021-03-14 11:54:25 +0000

    s

--- Program-5.c
+++ Program-5.c
@@ -10,7 +10,7 @@
 #include <stdio.h>
 int main()
 {
-  int c = 0, count = 0;
+  int c, count = 0;
   char s[1000];
 
   printf("Enter a string: ");
@@ -19,7 +19,7 @@ int main()
   for(c=0;c<500;c++)
   {
 
-     if(s[c]=='a'||s[c]=='A'||s[c]=='e'||s[c]=='E'||s[c]=='i'||s[c]=='I'||s[c]=='o'||s[c]=='0'|) 
+     if(s[c]=='a'||s[c]=='A'||s[c]=='e'||s[c]=='E'||s[c]=='i'||s[c]=='I'||s[c]=='o'||s[c]=='O'||s[c]=='u'||s[c]=='U') 
    {
    count++;
 
